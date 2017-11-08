b0355408f732f8cd0a5c6dfad6419e5ac52c9bb0
xvhuichuang <519401502@qq.com>
2017-11-08 16:28:30 +0800

    增加高阶函数测试

--- app/src/main/java/com/example/aml/gittext/Text.kt
+++ app/src/main/java/com/example/aml/gittext/Text.kt
@@ -6,5 +6,17 @@ import android.util.Log
  * Created by 18624915319 on 2017/11/5.
  */
 fun main(args: Array<String>) {
+    var str: String = local {
+        println("第一层接收")
+        return@local "第二层接收"
+    }
+    println(str)
 
+}
+
+//高阶函数
+fun <T : String> local(obj:() -> String): T {
+    var str = obj.invoke()
+    println(str)
+    return "第三层接收" as T
 }
