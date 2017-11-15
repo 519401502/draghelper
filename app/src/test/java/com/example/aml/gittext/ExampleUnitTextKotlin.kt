fcdb2323c9c13f8386c0d054ab4fe6b89befc65d
xvhuichuang <519401502@qq.com>
2017-11-15 17:23:46 +0800

    分支测试

--- app/src/test/java/com/example/aml/gittext/ExampleUnitTextKotlin.kt
+++ app/src/test/java/com/example/aml/gittext/ExampleUnitTextKotlin.kt
@@ -24,6 +24,7 @@ class ExampleUnitTextKotlin{
         httpUrlContent.setRequestProperty("Range", "bytes=1000-")
         httpUrlContent.connect()
 
+
         var length = httpUrlContent.contentLength
         val inputStream = httpUrlContent.getInputStream() ?: return
         val byteArray = ByteArrayOutputStream()
